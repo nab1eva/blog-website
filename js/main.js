79276920e5466890522d85ad40222e5832e8757a
abdulloxprogrammer <abdulloxr3545@gmail.com>
2023-02-03 02:14:59 -0800

    blog ready

--- js/main.js
+++ js/main.js
@@ -16,13 +16,27 @@ window.addEventListener("scroll", function () {
 
 
   function openNavbar() {
-	document.getElementById("navbar-responsive").style.left = "0";
-	// document.getElementById("navbar-responsive").style.top = "0";
+	// document.getElementById("navbar-responsive").style.left = "0";
+	document.getElementById("navbar-responsive").style.top = "0";
   }
   function closeNavbar() {
-	document.getElementById("navbar-responsive").style.left = "-100%";
-	// document.getElementById("navbar-responsive").style.top = "-100%";
+	// document.getElementById("navbar-responsive").style.left = "-100%";
+	document.getElementById("navbar-responsive").style.top = "-100%";
   }
   
   document.getElementById("navbar-open").addEventListener("click", openNavbar);
   document.getElementById("navbar-close").addEventListener("click", closeNavbar);
+
+  window.addEventListener("scroll", function () {
+    toggleBacktop();
+  });
+  
+  let backtop = document.getElementById("backtop");
+  
+  function toggleBacktop() {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+      backtop.style.opacity = 1;
+    } else {
+      backtop.style.opacity = 0;
+    }
+  }
